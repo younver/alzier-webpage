274ae3f8820394262b53d325604eb4cf2f52b10d
Yiğit Ö. Ünver <69584310+younver@users.noreply.github.com>
2022-08-05 16:50:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const mainFullpage = new fullpage("#fullpage", {
+  anchors: [
+    "page1",
+    "page2",
+    "page3",
+    "page4",
+    "page5",
+    "page6",
+    "page7",
+    "page8",
+  ],
+  sectionsColor: [
+    "#FFFFFF",
+    "#FF976A",
+    "#FFFFFF",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+  ],
+  navigation: true,
+  controlArrowColor: "#000000",
+  responsiveWidth: 1000,
+});
